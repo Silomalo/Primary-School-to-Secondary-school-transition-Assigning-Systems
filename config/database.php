288dce8ae981d22e81181140df4f62b1353e93c4
Silomalo Joseph <silomalojoseph@gmail.com>
2021-05-14 03:06:33 +0300

    char not functioning

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
             'url' => env(key: 'DATABASE_URL'),
             'host' => $DATABASE_URL["host"],
             'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], charlist: "/"),
+            'database' => ltrim($DATABASE_URL["path"], "/"),
             'username' => $DATABASE_URL["user"],
             'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
