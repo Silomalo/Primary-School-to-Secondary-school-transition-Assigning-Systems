dc49997e93388feed52644cd717d31b3db853586
Silomalo Joseph <silomalojoseph@gmail.com>
2021-05-14 03:20:03 +0300

    chnaged db settings

--- config/database.php
+++ config/database.php
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env(key: 'DATABASE_URL'),
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
+            'url' => env('DATABASE_URL'),
+            'host' => isset($DATABASE_URL["host"]) ? $DATABASE_URL["host"] : null,
+            'port' => isset($DATABASE_URL["port"]) ? $DATABASE_URL["port"] : null,
+            'database' => isset($DATABASE_URL["path"]) ? ltrim($DATABASE_URL["path"], "/") : null,
+            'username' => isset($DATABASE_URL["user"]) ? $DATABASE_URL["user"] : null,
+            'password' => isset($DATABASE_URL["pass"]) ? $DATABASE_URL["pass"] : null,
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
